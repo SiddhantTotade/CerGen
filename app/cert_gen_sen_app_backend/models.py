eeed750a364c53ed2681306852fddffd44ebc499
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-11-25 18:27:25 +0530

    created : models

--- app/cert_gen_sen_app_backend/models.py
+++ app/cert_gen_sen_app_backend/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Event(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=20,null=True,blank=True)
+    subject = models.CharField(max_length=20,null=True,blank=True)
+    from_date = models.DateField(auto_now_add=True)
+    to_date = models.DateField(auto_now_add=True,blank=True)
+    csv_file = models.FileField(upload_to='certificates/csv_files/')
+    certificates = models.FileField(upload_to='certificates/participant_certificates/')
+    slug = models.SlugField(null=True,blank=True)
+
+    def save(self, *args,**kwargs):
+        self.slug = slugify(self.event_name)
+        return super().save(*args,**kwargs)
+
+class Participant(models.Model):
+    event = models.ForeignKey(Event,on_delete=models.CASCADE)
--- app/cert_generator_sender_project/settings.py
+++ app/cert_generator_sender_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cert_gen_sen_app_backend',
 ]
 
 MIDDLEWARE = [
