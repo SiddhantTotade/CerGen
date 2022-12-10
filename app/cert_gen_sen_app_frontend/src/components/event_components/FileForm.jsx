b35c1603085d7dbd40065c5197b6468e96929c6d
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-12-10 18:03:41 +0530

    modified : form modal in file form

--- app/cert_gen_sen_app_frontend/src/components/event_components/FileForm.jsx
+++ app/cert_gen_sen_app_frontend/src/components/event_components/FileForm.jsx
@@ -5,21 +5,48 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
 
 export default function FileForm(props) {
+
+    const [file_focus, file_setFocused] = React.useState(false)
+    const [file_hasValue, file_setHasValue] = React.useState(false)
+    const file_onFocus = () => file_setFocused(true)
+    const file_onBlur = () => file_setFocused(false)
+
+    const [events, setEvents] = React.useState('');
+
+    const handleChange = (event) => {
+        setEvents(event.target.value);
+    };
 
     return (
         <div className='w-full'>
             <Dialog {...props} >
-                <DialogTitle>Add File</DialogTitle>
-                <DialogContent>
-                    <TextField autoFocus margin="dense" id="event_name" label="File" type="file" fullWidth variant="standard" />
-                </DialogContent>
+                <DialogTitle>Upload File</DialogTitle>
+                <FormControl sx={{ minWidth: 400, margin: 2 }}>
+                    <InputLabel id="events" >Events</InputLabel>
+                    <Select
+                        labelId="events"
+                        id="demo-simple-select"
+                        value={events}
+                        label="Events"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                    <TextField onFocus={file_onFocus} onBlur={file_onBlur} onChange={(e) => { if (e.target.value) file_setHasValue(true); else file_setHasValue(false); }} type={file_hasValue || file_focus ? "file" : "file"} autoFocus margin="dense" id="event_name" label="File" fullWidth variant="standard" />
+                </FormControl>
                 <DialogActions>
                     <Button onClick={props.onClose}>Cancel</Button>
-                    <Button >Create Event</Button>
+                    <Button >Upload File</Button>
                 </DialogActions>
             </Dialog>
-        </div>
+        </div >
     );
 }
