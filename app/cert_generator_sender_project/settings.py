c92c527dc545b8a5863cccdbf40e34cea521e664
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-01-18 20:37:23 +0530

    modified : settings.py

--- app/cert_generator_sender_project/settings.py
+++ app/cert_generator_sender_project/settings.py
@@ -30,8 +30,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'siddhanttotade.1994@gmail.com'
-EMAIL_HOST_PASSWORD = 'tqktmiiwpwpbuenk'
+EMAIL_HOST_USER = 'YOUR_EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR_PASSWORD'
 
 ALLOWED_HOSTS = ["*"]
 
