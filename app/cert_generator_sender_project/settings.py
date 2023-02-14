85279a9c4ba0328bd00b0e7653c9af0269a63ea3
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-02-14 15:49:10 +0530

    modified : settings.py

--- app/cert_generator_sender_project/settings.py
+++ app/cert_generator_sender_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
+from rest_framework.settings import api_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +84,16 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_KNOX = {
+  'SECURE_HASH_ALGORITHM': 'cryptography.hazmat.primitives.hashes.SHA512',
+  'AUTH_TOKEN_CHARACTER_LENGTH': 64,
+  'TOKEN_TTL': timedelta(days=1),
+  'USER_SERIALIZER': 'knox.serializers.UserSerializer',
+  'TOKEN_LIMIT_PER_USER': None,
+  'AUTO_REFRESH': False,
+  'EXPIRY_DATETIME_FORMAT': api_settings.DATETME_FORMAT,
+}
 
 WSGI_APPLICATION = 'cert_generator_sender_project.wsgi.application'
 
