565e9593d3bcaa3ba042cb96135c3275dee43456
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-02-27 23:21:21 +0530

    created : new model for admin

--- app/cert_gen_sen_app_backend/admin.py
+++ app/cert_gen_sen_app_backend/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from import_export.admin import ImportExportActionModelAdmin
 from .models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'email', 'name', 'tc', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        ('User Credentials', {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'tc')}),
+        ('permission', {'fields': ('is_admin',)})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'tc', 'password', 'password2')
+        }),
+    )
+
+    search_fields = ('email')
+    ordering = ('email', 'id')
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Event)
 admin.site.register(Participant)
 admin.site.register(EventFile)
--- app/cert_gen_sen_app_backend/models.py
+++ app/cert_gen_sen_app_backend/models.py
@@ -35,8 +35,38 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser):
-    email = models.EmailField
+    email = models.EmailField(
+        verbose_name="Email Address",
+        max_length=255,
+        unique=True
+    )
+
+    name = models.CharField(max_length=200)
+    tc = models.BooleanField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    object = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'tc']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perm(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 
 
 class Event(models.Model):
