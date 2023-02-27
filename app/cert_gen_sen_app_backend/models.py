7eb354c2d1b830b345496317a402e42aac48d220
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-02-27 21:12:03 +0530

    installed : jwt package

--- app/cert_gen_sen_app_backend/models.py
+++ app/cert_gen_sen_app_backend/models.py
@@ -1,48 +1,88 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.utils.text import slugify
 from django.utils.translation import gettext as _
 
 # Create your models here.
 
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, tc, password=None, password2=None):
+        if not email:
+            raise ValueError("Admin must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            tc=tc
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_super_user(self, email, name, tc, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+            tc=tc,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField
+
+
 class Event(models.Model):
-    id = models.AutoField(primary_key = True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    event_name = models.CharField(max_length=20,null=True,blank=True)
-    subject = models.CharField(max_length=250,null=True,blank=True)
-    event_department = models.CharField(max_length=20,null=True,blank=True) 
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=20, null=True, blank=True)
+    subject = models.CharField(max_length=250, null=True, blank=True)
+    event_department = models.CharField(max_length=20, null=True, blank=True)
     from_date = models.DateField()
     to_date = models.DateField()
-    event_year = models.IntegerField(blank=True,null=True)
-    slug = models.SlugField(null=True,blank=True)
+    event_year = models.IntegerField(blank=True, null=True)
+    slug = models.SlugField(null=True, blank=True)
 
-    def save(self, *args,**kwargs):
+    def save(self, *args, **kwargs):
         self.slug = slugify(self.event_name)
-        return super().save(*args,**kwargs)
-    
+        return super().save(*args, **kwargs)
+
     def __str__(self):
         return str(self.id)
 
+
 class EventFile(models.Model):
-    event_name = models.ForeignKey(Event,on_delete=models.CASCADE)
-    xlsx_file = models.FileField(upload_to='certificates/csv_files/',null=True,blank=True)
+    event_name = models.ForeignKey(Event, on_delete=models.CASCADE)
+    xlsx_file = models.FileField(
+        upload_to='certificates/csv_files/', null=True, blank=True)
+
 
 class Participant(models.Model):
-    event = models.ForeignKey(Event,on_delete=models.CASCADE)
-    student_name = models.CharField(max_length=50,null=True,blank=True)
-    student_id = models.CharField(max_length=50,null=True,blank=True)
-    email = models.EmailField(max_length=254,null=True,blank=True)
-    certificate_status = models.CharField(max_length=10,null=True,blank=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    student_name = models.CharField(max_length=50, null=True, blank=True)
+    student_id = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(max_length=254, null=True, blank=True)
+    certificate_status = models.CharField(max_length=10, null=True, blank=True)
+
 
 class CompletionCertificateTemplate(models.Model):
-    event = models.ForeignKey(Event,on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     template = models.ImageField(upload_to='completion_certificates_templates')
 
+
 class MeritCertificateTemplate(models.Model):
-    event = models.ForeignKey(Event,on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     template = models.ImageField(upload_to='merit_certificates_templates')
 
+
 class CompletionTemplateCoordinates(models.Model):
-    template = models.ForeignKey(Event,on_delete=models.CASCADE)
+    template = models.ForeignKey(Event, on_delete=models.CASCADE)
     x_coor_name = models.IntegerField()
     y_coor_name = models.IntegerField()
