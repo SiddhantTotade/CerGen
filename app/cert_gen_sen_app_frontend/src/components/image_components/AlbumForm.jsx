154f694fe937452fc7e0870d53c438897a2faefb
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-03-24 14:25:26 +0530

    modified : Album, AlbumForm and SpecificEvent

---
+++ app/cert_gen_sen_app_frontend/src/components/image_components/AlbumForm.jsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Box, Tab, Tabs } from "@mui/material";
+import PropTypes from "prop-types";
+import { Camera } from "../image_components/Camera";
+import { UploadParticipantImage } from "../image_components/UploadParticipantImage";
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 2 }}>
+                    <div>{children}</div>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        "aria-controls": `simple-tabpanel-${index}`,
+    };
+}
+
+export default function AlbumForm(props) {
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Dialog {...props} maxWidth='lg' >
+            <DialogTitle>Photo</DialogTitle>
+            <Box sx={{ width: "100%" }}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="basic tabs example"
+                    >
+                        <Tab label="Capture Photo" {...a11yProps(0)} />
+                        <Tab label="Upload Photo" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+                    <Camera participant={props.participant} onClose={props.onClose} />
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    <UploadParticipantImage onClose={props.onClose} />
+                </TabPanel>
+            </Box>
+        </Dialog>
+    );
+}
