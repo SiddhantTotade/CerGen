d60864f087817a9c4a6bc98bf505d1f6c420956d
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-04-07 19:50:38 +0530

    modified : FileForm

--- app/cert_gen_sen_app_frontend/src/components/event_components/FileForm.jsx
+++ app/cert_gen_sen_app_frontend/src/components/event_components/FileForm.jsx
@@ -16,18 +16,18 @@ import { useGetAllEventsQuery } from "../../services/eventsAPI";
 import { getToken } from "../../services/LocalStorageService";
 
 export default function FileForm(props) {
-  // const [eventsData, setEventsData] = useState([]);
-  const [openSpinner, setOpenSpinner] = useState(false);
-  const [openSnack, setOpenSnack] = useState(false);
-  const [message, setMessage] = useState("");
-  const [alertType, setAlertType] = useState("");
+  const [snackAndSpinner, setSnackAndSpinner] = useState({
+    openSpinner: true,
+    openSnack: true,
+    message: "",
+    alertType: "success"
+  })
 
   const { access_token } = getToken()
 
   const [participantData, responseUploadParticipants] = useUploadParticipantsMutation()
-  const { data = [], isLoading } = useGetAllEventsQuery(access_token)
 
-  const [events, setEvents] = React.useState("");
+  const { data = [], isLoading } = useGetAllEventsQuery(access_token)
 
   const [participantsFileData, setParticipantsFileData] = React.useState({
     event_id: "",
@@ -35,63 +35,78 @@ export default function FileForm(props) {
   });
 
   function handleCloseSnackbar() {
-    setOpenSnack(false);
+    setSnackAndSpinner({ openSnack: false });
   }
 
+  React.useEffect(() => {
+    if (!props.open) {
+      setParticipantsFileData({ event_id: "", participants_file: null })
+    }
+  }, [props.open])
+
   return (
-    <div className="w-full">
-      <Dialog {...props}>
-        <DialogTitle>Upload File</DialogTitle>
-        <FormControl sx={{ minWidth: 400, margin: 2, gap: 2 }}>
-          <InputLabel id="events">Choose Event</InputLabel>
-          <Select
-            labelId="events"
-            id='event_id'
-            name="event_id"
-            value={events}
-            variant="standard"
-            type="text"
-            label="Events"
-            onChange={(e) => { setParticipantsFileData({ ...participantsFileData, event_id: e.target.value }) }}
-          >
-            {data !== undefined ? (
-              Object.values(data).map((events) => {
-                return (
-                  <MenuItem value={events.id} key={events.id}>
-                    {events.event_name}
-                  </MenuItem>
-                );
-              })
-            ) : (
-              <MenuItem value={events.id} key={events.id}>
-                No Data Available
-              </MenuItem>
-            )}
-          </Select>
-          <TextField
-            inputProps={{ accept: ".xlsx, .xls" }}
-            onChange={(e) => { setParticipantsFileData({ ...participantsFileData, participants_file: e.target.files[0] }) }}
-            autoFocus
-            margin="dense"
-            id="participants_file"
-            type='file'
-            fullWidth
-            variant="standard"
-          />
-        </FormControl>
-        <DialogActions>
-          <Button onClick={props.onClose}>Cancel</Button>
-          <Button onClick={() => participantData({ access_token: access_token, participantsFileData: participantsFileData })}>Upload File</Button>
-        </DialogActions>
-      </Dialog>
-      <BackdropSpinner open={openSpinner} />
-      <AlertSnackbar
-        open={openSnack}
-        message={message}
-        severity={alertType}
-        onClose={handleCloseSnackbar}
-        autoHideDuration={6000}
-      />
-    </div>
+    <>
+      {
+        responseUploadParticipants.isLoading ?
+          <>
+            <BackdropSpinner open={snackAndSpinner.openSpinner} />
+          </> : <div className="w-full">
+            <Dialog {...props}>
+              <DialogTitle>Upload File</DialogTitle>
+              <FormControl sx={{ minWidth: 400, margin: 2, gap: 2 }}>
+                <InputLabel id="events">Choose Event</InputLabel>
+                <Select
+                  labelId="events"
+                  id='event_id'
+                  name="event_id"
+                  value={participantsFileData.event_id}
+                  variant="standard"
+                  type="text"
+                  label="Events"
+                  onChange={(e) => { setParticipantsFileData({ ...participantsFileData, event_id: e.target.value }) }}
+                >
+                  {data !== undefined ? (
+                    Object.values(data).map((events) => {
+                      return (
+                        <MenuItem value={events.id} key={events.id}>
+                          {events.event_name}
+                        </MenuItem>
+                      );
+                    })
+                  ) : (
+                    <MenuItem value={null} key={null}>
+                      No Data Available
+                    </MenuItem>
+                  )}
+                </Select>
+                <TextField
+                  inputProps={{ accept: ".xlsx, .xls" }}
+                  onChange={(e) => { setParticipantsFileData({ ...participantsFileData, participants_file: e.target.files[0] }) }}
+                  autoFocus
+                  margin="dense"
+                  id="participants_file"
+                  type='file'
+                  fullWidth
+                  variant="standard"
+                />
+              </FormControl>
+              <DialogActions>
+                <Button onClick={props.onClose}>Cancel</Button>
+                <Button onClick={() => { participantData({ access_token: access_token, participantsFileData: participantsFileData }); setParticipantsFileData({ event_id: "", participants_file: null }) }}>Upload File</Button>
+              </DialogActions>
+            </Dialog>
+            {
+              responseUploadParticipants.data ?
+                <AlertSnackbar
+                  open={snackAndSpinner.openSnack}
+                  message={responseUploadParticipants.data}
+                  severity={snackAndSpinner.alertType}
+                  onClose={handleCloseSnackbar}
+                  autoHideDuration={6000}
+                /> : ""
+            }
+          </div>
+      }
+    </>
   );
 }
