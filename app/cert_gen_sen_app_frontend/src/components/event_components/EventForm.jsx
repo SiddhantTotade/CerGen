21b60c399c5fcba58c42b18a31b6fba5e6fd7cc5
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-04-08 15:12:31 +0530

    modified : EventForm

--- app/cert_gen_sen_app_frontend/src/components/event_components/EventForm.jsx
+++ app/cert_gen_sen_app_frontend/src/components/event_components/EventForm.jsx
@@ -17,35 +17,21 @@ import { getToken } from '../../services/LocalStorageService';
 import { useGetLoggedInUserQuery } from '../../services/userAuthAPI';
 import { useDispatch } from 'react-redux';
 import { setUserInfo } from '../../features/userSlice';
-import { useSelector } from 'react-redux';
 
 export default function EventForm(props) {
-    const [openSnack, setOpenSnack] = useState(false)
-    const [message, setMessage] = useState("")
-    const [alertType, setAlertType] = useState("")
-    const [openSpinner, setOpenSpinner] = useState(false)
-
-    const [from_focus, from_setFocused] = React.useState(false)
-    const [from_hasValue, from_setHasValue] = React.useState(false)
-    const from_onFocus = () => from_setFocused(true)
-    const from_onBlur = () => from_setFocused(false)
-
-    const [to_focus, to_setFocused] = React.useState(false)
-    const [to_hasValue, to_setHasValue] = React.useState(false)
-    const to_onFocus = () => to_setFocused(true)
-    const to_onBlur = () => to_setFocused(false)
 
     const { access_token } = getToken()
 
+    const [snackAndSpinner, setSnackAndSpinner] = useState({
+        openSpinner: true,
+        openSnack: true,
+        message: "",
+        alertType: "success"
+    })
+
     const { data = [], isSuccess } = useGetLoggedInUserQuery(access_token)
 
-    const dispatch = useDispatch()
-
-    React.useEffect(() => {
-        if (data && isSuccess) {
-            dispatch(setUserInfo({ email: data.email, name: data.name }))
-        }
-    }, [data, isSuccess, dispatch])
+    const [createEvent, responseCreateEvent] = useCreateEventMutation()
 
     const [eventData, setEventData] = useState({
         user: data.id,
@@ -57,9 +43,34 @@ export default function EventForm(props) {
         event_year: "",
     })
 
-    const [createEvent, responseCreateEvent] = useCreateEventMutation()
+    const [from_focus, from_setFocused] = React.useState(false)
+    const [from_hasValue, from_setHasValue] = React.useState(false)
+    const from_onFocus = () => from_setFocused(true)
+    const from_onBlur = () => from_setFocused(false)
+
+    const [to_focus, to_setFocused] = React.useState(false)
+    const [to_hasValue, to_setHasValue] = React.useState(false)
+    const to_onFocus = () => to_setFocused(true)
+    const to_onBlur = () => to_setFocused(false)
+
+    const dispatch = useDispatch()
+
+    React.useEffect(() => {
+        if (data && isSuccess) {
+            dispatch(setUserInfo({ email: data.email, name: data.name }))
+        }
+    }, [data, isSuccess, dispatch])
 
     const [eventYear, setEventYear] = useState("")
+
+    let maxOffset = 20;
+    let thisYear = (new Date()).getFullYear();
+    let allYears = [];
+    for (let x = 0; x <= maxOffset; x++) {
+        allYears.push(thisYear - x)
+    }
+
+    const yearList = allYears.map((x) => { return <MenuItem value={x} key={x}>{x}</MenuItem> });
 
     function handleEventData(event) {
         const newData = { ...eventData }
@@ -69,49 +80,47 @@ export default function EventForm(props) {
 
     const handleEventYear = (event) => {
         setEventYear(event.target.value)
-        eventData.event_year = event.target.value
+        setEventData({ event_year: event.target.value })
     }
-
-    let maxOffset = 10;
-    let thisYear = (new Date()).getFullYear();
-    let allYears = [];
-    for (let x = 0; x <= maxOffset; x++) {
-        allYears.push(thisYear - x)
-    }
-
-    const yearList = allYears.map((x) => { return <MenuItem value={x} key={x}>{x}</MenuItem> });
 
     function handleCloseSnackbar() {
-        setOpenSnack(false)
+        setSnackAndSpinner({ openSnack: false })
     }
 
     return (
-        <div className='w-full'>
-            <Dialog {...props} >
-                <DialogTitle>Create Event</DialogTitle>
-                <DialogContent>
-                    <TextField onChange={(e) => handleEventData(e)} value={eventData.event_name} autoFocus margin="dense" id="event_name" label="Event Name" type="text" fullWidth variant="standard" />
-                    <TextField onChange={(e) => handleEventData(e)} value={eventData.subject} autoFocus margin="dense" id="subject" label="Event Subject" type="text" fullWidth variant="standard" />
-                    <TextField onChange={(e) => handleEventData(e)} value={eventData.event_department} autoFocus margin="dense" id="event_department" label="Event Department" type="text" fullWidth variant="standard" />
-                    <TextField onFocus={from_onFocus} onBlur={from_onBlur} onChange={(e) => { handleEventData(e); if (e.target.value) from_setHasValue(true); else from_setHasValue(false); }} type={from_hasValue || from_focus ? "date" : "text"} value={eventData.from_date} autoFocus margin="dense" id="from_date" label="Event - From Date" fullWidth variant="standard" />
-                    <TextField onFocus={to_onFocus} onBlur={to_onBlur} onChange={(e) => { handleEventData(e); if (e.target.value) to_setHasValue(true); else to_setHasValue(false); }} type={to_hasValue || to_focus ? 'date' : 'text'} value={eventData.to_date} autoFocus margin="dense" id="to_date" label="Event - To Date" fullWidth variant="standard" />
-                    <FormControl variant="standard" sx={{ width: "100%" }}  >
-                        <InputLabel id="demo-simple-select-label" variant='standard' >Choose Event Year</InputLabel>
-                        <Select labelId="demo-simple-select-label" id="demo-simple-select" variant="standard" onChange={handleEventYear} value={eventYear}>
-                            <MenuItem value="" key="" >
-                                <em>None</em>
-                            </MenuItem>
-                            {yearList}
-                        </Select>
-                    </FormControl>
-                </DialogContent>
-                <DialogActions>
-                    <Button variant='contained' onClick={props.onClose}>Cancel</Button>
-                    <Button variant='contained' onClick={() => createEvent({ access_token: access_token, eventData: eventData })} >Create</Button>
-                </DialogActions>
-            </Dialog>
-            <BackdropSpinner open={openSpinner} />
-            <AlertSnackbar open={openSnack} message={message} severity={alertType} onClose={handleCloseSnackbar} autoHideDuration={6000} />
-        </div>
+        <>
+            {
+                responseCreateEvent.isLoading ?
+                    <BackdropSpinner open={snackAndSpinner.openSpinner} /> :
+                    <div className='w-full'>
+                        <Dialog {...props} >
+                            <DialogTitle>Create Event</DialogTitle>
+                            <DialogContent>
+                                <TextField onChange={(e) => handleEventData(e)} value={eventData.event_name} autoFocus margin="dense" id="event_name" label="Event Name" type="text" fullWidth variant="standard" />
+                                <TextField onChange={(e) => handleEventData(e)} value={eventData.subject} autoFocus margin="dense" id="subject" label="Event Subject" type="text" fullWidth variant="standard" />
+                                <TextField onChange={(e) => handleEventData(e)} value={eventData.event_department} autoFocus margin="dense" id="event_department" label="Event Department" type="text" fullWidth variant="standard" />
+                                <TextField onFocus={from_onFocus} onBlur={from_onBlur} onChange={(e) => { handleEventData(e); if (e.target.value) from_setHasValue(true); else from_setHasValue(false); }} type={from_hasValue || from_focus ? "date" : "text"} value={eventData.from_date} autoFocus margin="dense" id="from_date" label="Event - From Date" fullWidth variant="standard" />
+                                <TextField onFocus={to_onFocus} onBlur={to_onBlur} onChange={(e) => { handleEventData(e); if (e.target.value) to_setHasValue(true); else to_setHasValue(false); }} type={to_hasValue || to_focus ? 'date' : 'text'} value={eventData.to_date} autoFocus margin="dense" id="to_date" label="Event - To Date" fullWidth variant="standard" />
+                                <FormControl variant="standard" sx={{ width: "100%" }}  >
+                                    <InputLabel id="demo-simple-select-label" variant='standard' >Choose Event Year</InputLabel>
+                                    <Select labelId="demo-simple-select-label" id="demo-simple-select" variant="standard" onChange={handleEventYear} value={eventYear}>
+                                        <MenuItem value="" key="" >
+                                            <em>None</em>
+                                        </MenuItem>
+                                        {yearList}
+                                    </Select>
+                                </FormControl>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button variant='contained' onClick={props.onClose}>Cancel</Button>
+                                <Button variant='contained' onClick={() => createEvent({ access_token: access_token, eventData: eventData })} >Create</Button>
+                            </DialogActions>
+                        </Dialog>
+                        {
+                            responseCreateEvent.data ?
+                                <AlertSnackbar open={snackAndSpinner.openSnack} message={snackAndSpinner.message} severity={snackAndSpinner.alertType} onClose={handleCloseSnackbar} autoHideDuration={6000} /> : ""
+                        }
+                    </div>}
+        </>
     );
 }
