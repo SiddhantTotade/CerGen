86c11209a959fe0e08ec9c6d5e72a1bd139587a9
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-04-25 16:30:01 +0530

    modified : Home.js

--- app/cert_gen_sen_app_frontend/src/Home.js
+++ app/cert_gen_sen_app_frontend/src/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Sidebar from "./components/base_components/Sidebar";
-import HomeAnimation from "./pages/HomeAnimation";
 
 export default function Home() {
   return (
     <>
       <Sidebar />
-      {/* <HomeAnimation /> */}
     </>
   );
 }
