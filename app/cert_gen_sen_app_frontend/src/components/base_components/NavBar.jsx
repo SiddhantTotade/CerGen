3a3cb0661914998736868b66806a4d74ee8f9a9d
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-04-27 20:13:41 +0530

    modified : NavBar and UserDetails

--- app/cert_gen_sen_app_frontend/src/components/base_components/NavBar.jsx
+++ app/cert_gen_sen_app_frontend/src/components/base_components/NavBar.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material'
 import { NavLink } from 'react-router-dom'
-import { getToken } from '../../services/LocalStorageService'
 
 const NavBar = () => {
-
-    const { access_token } = getToken()
 
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position='static' color='primary' >
                 <Toolbar>
-                    <Typography variant='h5' component='div' sx={{ flexGrow: 1 }} >CerGen</Typography>
+                    <Typography variant='h5' component='div' sx={{ flexGrow: 1, fontFamily: 'Bruno Ace' }} >CerGen</Typography>
                     <Button style={({ isActive }) => { return { backgroundColor: isActive ? "#026ab7" : "" } }} component={NavLink} to='/api/login' sx={{ color: "white", textTransform: "None" }} >Sign In</Button>
                     <Button style={({ isActive }) => { return { backgroundColor: isActive ? "#026ab7" : "" } }} component={NavLink} to='/api/register' sx={{ color: "white", textTransform: "None" }} >Sign Up</Button>
-                    {/* {
-                        access_token ? <Button style={({ isActive }) => { return { backgroundColor: isActive ? "#6d1b7b" : "" } }} component={NavLink} to='/home' sx={{ color: "white", textTransform: "None" }} >Home</Button> : <Button style={({ isActive }) => { return { backgroundColor: isActive ? "#6d1b7b" : "" } }} component={NavLink} to="/login" sx={{ color: "white", textTransform: "None" }} >Login / Register</Button>
-                    } */}
                 </Toolbar>
             </AppBar>
         </Box>
