57a0d9b100c53908b0af7f2ad251953427772eb0
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-05-21 13:05:19 +0530

    modified : settings

--- app/cert_generator_sender_project/settings.py
+++ app/cert_generator_sender_project/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 6
 }
 
