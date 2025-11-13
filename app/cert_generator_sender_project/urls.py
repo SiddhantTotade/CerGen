a584556aa33c56dafd7008dea89e58d60a1de6e0
SiddhantTotade <siddhanttotade.1994@gmail.com>
2025-11-13 20:43:07 +0000

    feat : urls.py

--- app/cert_generator_sender_project/urls.py
+++ app/cert_generator_sender_project/urls.py
@@ -6,7 +6,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # REST APIs -- For authentication and template generation
     path("auth/api/", include("cergen_auth.urls")),
-    path("graphql/", include("cert_gen_sen_app_backend.urls")),
     path("app/api/", include("cert_gen_sen_app_backend.urls")),
+
+    # GraphQL APIs -- For application
+    path("graphql/", include("cert_gen_sen_app_backend.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
